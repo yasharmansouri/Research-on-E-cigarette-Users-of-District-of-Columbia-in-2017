9fda8d93ffefd731cf9fede2359e73646e98195c
ymans88 <yasharmansouri99@gmail.com>
2019-10-10 12:43:05 -0400

    notebook cleaed, functions written, visualizations done

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,39 +1,33 @@
-"""
-This module is for your data cleaning.
-It should be repeatable.
+import  pandas as pd
 
-## PRECLEANING
-There should be a separate script recording how you transformed the json api calls into a dataframe and csv.
+def full_clean(df):
+    """ for data cleaning, the function 
+    drops the unnecessary columns by keeping/reordering the required ones for better analysis purposes\n
+    drops null values based on the data_value and sample_size\n
+    drops 2 consecutive years/cumulative data\n
+    changes the the data type in the specified columns\n
+    creates a column of count in case further analysis requires the average number of people """
 
-## SUPPORT FUNCTIONS
-There can be an unlimited amount of support functions.
-Each support function should have an informative name and return the partially cleaned bit of the dataset.
-"""
-import pandas as pd
-
-
-def drop_columns (df):
-    df = df[['year', 'locationabbr', 'locationdesc', 'topicdesc', 'measuredesc','age', 'gender', 'race', 'education', 'response', 'sample_size', 'data_value', 'data_value_std_err', 'low_confidence_limit', 'high_confidence_limit']]
-    pass
-
-def support_function_two(example):
-    pass
-
-def support_function_three(example):
-    pass
-
-def full_clean():
-    """
-    This is the one function called that will run all the support functions.
-    Assumption: Your data will be saved in a data folder and named "dirty_data.csv"
-
-    :return: cleaned dataset to be passed to hypothesis testing and visualization modules.
-    """
-    dirty_data = pd.read_csv("./data/dirty_data.csv")
-
-    cleaning_data1 = support_function_one(dirty_data)
-    cleaning_data2 = support_function_two(cleaning_data1)
-    cleaned_data= support_function_three(cleaning_data2)
-    cleaned_data.to_csv('./data/cleaned_for_testing.csv')
+    df = df[['year', 'locationabbr', 'locationdesc', 'topicdesc', 'measuredesc', 'age', 'gender', 'race', 'education',
+         'response', 'sample_size', 'data_value', 'data_value_std_err', 'low_confidence_limit', 'high_confidence_limit']]
     
-    return cleaned_data
+    df = df.dropna(subset=['data_value', 'sample_size'])
+    
+    year_idx_drop = df.query('(year == "2011-2012") | (year == "2012-2013") |\
+    (year == "2013-2014") | (year == "2014-2015") | (year == "2015-2016") | (year == "2016-2017")').index
+    
+    df = df.drop(year_idx_drop)
+    
+    df[['sample_size', 'data_value', 'data_value_std_err',
+    'low_confidence_limit', 'high_confidence_limit']] = df[['sample_size', 'data_value', 'data_value_std_err',
+                                                            'low_confidence_limit', 'high_confidence_limit']].astype(float)
+    df[['year']] = df[['year']].astype(int)
+     
+    df['count'] = df.data_value * (df.sample_size / 100)
+    
+    return df
+
+
+
+    
+    
